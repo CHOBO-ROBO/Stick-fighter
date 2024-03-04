009e0cb79068c8dd64f7ffc87b8aeff816c1dc49
CHOBO_ROBO <lightingthunder0617@gmail.com>
2024-03-04 18:10:20 +1300

    now walking
    yes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,20 @@ public class PlayerMovement : MonoBehaviour
   [SerializeField, Range(0, 100)] float movementSpeed = 5f;
   [SerializeField] CinemachineVirtualCamera virtualCamera;
 
+  Animator animator;
+
+  float deadZone = 0.1f;
+
+  void Start()
+  {
+    Transform model = transform.Find("kid");
+    animator = model.GetComponent<Animator>();
+  }
+
   void Update()
   {
     float xAxis = Input.GetAxis("Horizontal");
-    Debug.Log($"xAxis: {xAxis}");
+    // Debug.Log($"xAxis: {xAxis}");
     float zAxis = Input.GetAxis("Vertical");
     Debug.Log($"zAxis: {zAxis}");
 
@@ -19,5 +29,22 @@ public class PlayerMovement : MonoBehaviour
     transform.Translate(movementSpeed * Time.deltaTime * movement);
 
     transform.eulerAngles = new Vector3(0, virtualCamera.transform.eulerAngles.y, 0);
+
+    if (Input.GetAxis("Vertical") >= deadZone)
+    {
+      animator.ResetTrigger("onStop");
+      animator.ResetTrigger("onBackwards");
+      animator.SetTrigger("onWalk");
+    }
+    else if (Input.GetAxis("Vertical") <= -deadZone)
+    {
+      animator.ResetTrigger("onStop");
+      animator.ResetTrigger("onWalk");
+      animator.SetTrigger("onBackwards");
+    }
+    else
+    {
+      animator.SetTrigger("onStop");
+    }
   }
 }
